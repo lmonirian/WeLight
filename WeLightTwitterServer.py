c0a2358c2e60b1c50277ed31eb6d3e39a4b28a3c
Luis Ceze <luisceze@cs.washington.edu>
2015-06-09 22:21:32 -0700

    token management and permissions

--- WeLightTwitterServer.py
+++ WeLightTwitterServer.py
@@ -1,14 +1,51 @@
 import nltk
 import sys
+import sqlite3
+
 from callapi import *
 
 
-aToken='dnZhMGZ5Q3VBc2ltbGZNaFAyTWJ2UG9NMmloK1dsSFFOV0gyMTczRzhXVT0='
-bId='001788fffe174db2'
+# lab
+# aToken='dnZhMGZ5Q3VBc2ltbGZNaFAyTWJ2UG9NMmloK1dsSFFOV0gyMTczRzhXVT0='
+# bId='001788fffe174db2'
 
-print getPhilipsHueInfo(aToken, bId)
+# home
+# aToken='MWRuUkdndGVUc2VnTjJIZW1FWXVzOFYxZmFFNUlWNFlQOTB4VllmSlkxTT0='
+# bId='001788fffe17b990'
 
-philipsControlCustom(constructCustomMsg("lights/0/state", '{"on":true}', "PUT", bId), aToken)
-philipsControlCustom(constructCustomMsg("lights/1/state", '{"on":true}', "PUT", bId), aToken)
-philipsControlCustom(constructCustomMsg("lights/2/state", '{"on":true}', "PUT", bId), aToken)
-philipsControlCustom(constructCustomMsg("lights/3/state", '{"on":true}', "PUT", bId), aToken)
+conn = sqlite3.connect('db/welight.db')
+
+def getTokenAndBridgeId(username):
+    c = conn.cursor()
+    c.execute("select * from usertokens where userhandle like'"+username+"'")
+    row = c.fetchone()
+
+    if row == None:
+        return ("","")
+    
+    return (row[2],row[1])
+
+def checkPermission(userFrom, userTo):
+    c = conn.cursor()
+    c.execute("select * from userperms where dest='"+userTo+"' and src='"+userFrom+"'")
+    row = c.fetchone()
+
+    if row == None:
+        return False
+    return True
+
+def setAllLightsToColor(aToken, bId, xy):
+    jsonHueInfo = getPhilipsHueInfo(t, b)
+    print jsonHueInfo
+    for l in jsonHueInfo["lights"]:
+        philipsControlCustom(constructCustomMsg("lights/"+l+"/state", '{"on":true}', "PUT", bId), aToken)
+        philipsControlCustom(constructCustomMsg("lights/"+l+"/state", '{"xy":'+xy+'}', "PUT", bId), aToken)
+
+
+srcUser = "we_Light_"
+destUser = "luisceze"
+
+if checkPermission(srcUser, destUser):
+    (t, b) = getTokenAndBridgeId(destUser)    
+    setAllLightsToColor(t, b, "[0.32,0.1]")
+
